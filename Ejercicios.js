f8bfacef8a1c8bc4de1cdebce9103e8f73f1ec12
LautaroNRosales <emanueldayer69@gmail.com>
2025-04-08 20:03:21 -0300

    ejercicio 1 al 3

---
+++ Ejercicios.js
@@ -0,0 +1,87 @@
+// Actividad 1
+
+let primerNum = 2;
+let SegundoNum = 1;
+SumaNum = primerNum + SegundoNum;
+console.log(`${primerNum} + ${SegundoNum} es ${SumaNum}`);
+
+// Actividad 2
+
+let valoruno = 8;
+let valordos = 3;
+let valortres = 1;
+let mayor = 0;
+let menor = 0;
+
+if (valoruno > valordos & valoruno > valortres)
+{
+    if(valordos > valortres)
+        {
+            menor = valortres
+        }
+        else
+        {
+            menor = valordos
+        }
+    mayor = valoruno
+} 
+else if (valordos > valoruno & valordos > valortres)
+{
+    if(valortres > valoruno)
+        {
+            menor = valoruno
+        }
+        else
+        {
+            menor = valortres
+        }
+    mayor = valordos
+}
+else
+{
+    if(valordos > valoruno)
+        {
+            menor = valoruno
+        }
+        else
+        {
+            menor = valordos
+        }
+    mayor = valortres
+}
+
+console.log(`Mayor: ${mayor} y Menor ${menor}`);
+
+// Actividad 3
+
+var palabra = "hola"
+console.log(`la palabra ${palabra} tiene letras ${palabra.length}`);
+
+// Actividad 4
+
+// Actividad 5
+
+// Actividad 6
+
+// Actividad 7
+
+// Actividad 8
+
+// Actividad 9
+
+// Actividad 10
+
+
+// Actividad 11
+
+
+// Actividad 12
+
+// Actividad 13
+
+
+// Actividad 14
+
+// Actividad 15
+
+// Actividad 16
